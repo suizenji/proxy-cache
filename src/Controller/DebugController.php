0c200ccef3d1a89045a3e96d305e1b066087b068
suizenji <suizenji@pop-os.localdomain>
2022-09-09 12:54:56 +0900

    debug-net

--- src/Controller/DebugController.php
+++ src/Controller/DebugController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Util\Dns;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use function Symfony\Component\String\u;
 
 #[Route('/_debug', name: 'app_debug_')]
 class DebugController extends AbstractController
@@ -19,6 +22,22 @@ class DebugController extends AbstractController
         $contents = [
             'host' => $host,
             'ip' => $ip,
+        ];
+
+        return $this->render('debug/index.html.twig', ['contents' => $contents]);
+    }
+
+    #[Route('/net', name: 'net')]
+    public function net(Request $request): Response
+    {
+        $schemeDomain = u($request->getSchemeAndHttpHost())
+                    ->replaceMatches('/^(.*)(:[0-9]+)$/', function ($match) {
+                        return $match[1];
+                    });
+
+        $contents = [
+            'scheme_domain' => $schemeDomain,
+            'path' => $request->getPathInfo(),
         ];
 
         return $this->render('debug/index.html.twig', ['contents' => $contents]);
