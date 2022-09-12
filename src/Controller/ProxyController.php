7359e37893159a9298b6e505cf8061f4ea6600fd
suizenji <suizenji@pop-os.localdomain>
2022-09-12 12:16:10 +0900

    proxy

--- src/Controller/ProxyController.php
+++ src/Controller/ProxyController.php
@@ -2,20 +2,71 @@
 
 namespace App\Controller;
 
+use function Symfony\Component\String\u;
+use App\Util\Dns;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProxyController extends AbstractController
 {
     #[Route('/proxy', name: 'app_proxy')]
-    public function index(Request $request): JsonResponse
+    public function index(Request $request, HttpClientInterface $client): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProxyController.php',
-            'uri' => $request->getUri()
+        $method = $request->getMethod();
+
+        $host = $request->getHost();
+        $domain = (string) u($host)->replaceMatches('/^(.*)(:[0-9]+)$/', function ($match) {
+            return $match[1];
+        });
+
+        $ip = Dns::getA($domain);
+#        $schemeAndHttpHost = $request->getSchemeAndHttpHost();
+#        $schemeAndIp = (string) u($schemeAndHttpHost)->replace($domain, $ip);
+        $schemeAndIp = $request->getScheme() . '://' . $ip;
+
+        $path = $request->getPathInfo();
+        $query = $request->getQueryString();
+        $uri = $schemeAndIp . $path . '?' . $query;
+
+        $headers = $request->server->getHeaders();
+        unset($headers['content-length']);
+        unset($headers['Content-length']);
+        unset($headers['Content-Length']);
+        unset($headers['CONTENT_LENGTH']);
+        unset($headers['host']);
+        $headers['HOST'] = $host;
+        unset($headers['connection']);
+        $headers['Connection'] = 'close';
+
+        /** @var ResponseInterface $response */
+        $response = $client->request($method, $uri, [
+            'headers' => $headers,
+            'body' => $request->getContent(),
+            'verify_host' => false,
         ]);
+
+        $content = $response->getContent();
+        $status = $response->getStatusCode();
+        $headers = $response->getHeaders();
+
+        unset($headers['content-encoding']);
+        unset($headers['Content-encoding']);
+        unset($headers['Content-Encoding']);
+        unset($headers['CONTENT_ENCODING']);
+        unset($headers['content-length']);
+        unset($headers['Content-length']);
+        unset($headers['Lontent-Length']);
+        unset($headers['LONTENT_LENGTH']);
+        unset($headers['transfer-encoding']);
+        unset($headers['Transfer-encoding']);
+        unset($headers['Transfer-Encoding']);
+        unset($headers['TRANSFER_ENCODING']);
+        $headers['HOST'] = $host;
+
+        return new Response($content, $status, $headers);
     }
 }
