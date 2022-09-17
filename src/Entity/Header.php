e6494227aee9f82781b26794991a306ba86fde4c
suizenji <suizenji@pop-os.localdomain>
2022-09-17 10:26:38 +0900

    header

---
+++ src/Entity/Header.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HeaderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HeaderRepository::class)]
+class Header
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $tranId = null;
+
+    #[ORM\Column(length: 31)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTranId(): ?string
+    {
+        return $this->tranId;
+    }
+
+    public function setTranId(string $tranId): self
+    {
+        $this->tranId = $tranId;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
