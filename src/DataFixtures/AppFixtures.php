82c385d69d6006d17a7d1fb70e4644c4c8e46182
suizenji <suizenji@pop-os.localdomain>
2022-09-18 11:23:32 +0900

    small fix

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,43 +17,28 @@ class AppFixtures extends Fixture
         $tranId = '123abc';
         $createdAt = new \DateTimeImmutable();
 
-        self::recordRequestContext($manager, $tranId, createdAt: $createdAt);
+        self::recordContext($manager, $tranId, Http::TYPE_SEND, 'GET', '/', 'HTTP/1.1', $createdAt);
         self::recordHeaders($manager, $tranId, Http::TYPE_SEND, [
             'HOST' => 'localhost',
             'User-Agent' => 'Nginx',
         ]);
         self::recordBody($manager, $tranId, Http::TYPE_SEND);
 
-        self::recordResponseContext($manager, $tranId, createdAt: $createdAt);
+        self::recordContext($manager, $tranId, Http::TYPE_RECV, 'HTTP/1.1', '200', 'OK', $createdAt);
         self::recordHeaders($manager, $tranId, Http::TYPE_RECV, [
             'Content-Length' => '6',
         ]);
         self::recordBody($manager, $tranId, Http::TYPE_RECV, 'foo');
     }
 
-    private static function recordRequestContext($manager, $tranId, $method = 'GET', $uri = '/', $version = 'HTTP/1.1', $createdAt = new \DateTimeImmutable())
+    private static function recordContext($manager, $tranId, $type, $f1, $f2, $f3, $createdAt)
     {
         $entity = (new HttpContext)
             ->setTranId($tranId)
-            ->setType(Http::TYPE_SEND)
-            ->setF1($method)
-            ->setF2($uri)
-            ->setF3($version)
-            ->setCreatedAt($createdAt)
-            ;
-
-        $manager->persist($entity);
-        $manager->flush();
-    }
-
-    private static function recordResponseContext($manager, $tranId, $version = 'HTTP/1.1', $status = 200, $message = 'OK', $createdAt = new \DateTimeImmutable())
-    {
-        $entity = (new HttpContext)
-            ->setTranId($tranId)
-            ->setType(Http::TYPE_RECV)
-            ->setF1($version)
-            ->setF2($status)
-            ->setF3($message)
+            ->setType($type)
+            ->setF1($f1)
+            ->setF2($f2)
+            ->setF3($f3)
             ->setCreatedAt($createdAt)
             ;
 
