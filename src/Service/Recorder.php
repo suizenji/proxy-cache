82637ca2431e74fd76ee8a093d2fe6e5d918d0a3
suizenji <suizenji@pop-os.localdomain>
2022-09-19 18:16:27 +0900

    record request

--- src/Service/Recorder.php
+++ src/Service/Recorder.php
@@ -6,31 +6,23 @@ use App\Entity\Http;
 use App\Entity\HttpContext;
 use App\Entity\HttpHeader;
 use App\Entity\HttpBody;
-use App\Repository\HttpContextRepository;
-use App\Repository\HttpHeaderRepository;
-use App\Repository\HttpBodyRepository;
-#use Doctrine\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class Recorder
 {
     public function __construct(
-#        private HttpContextRepository $repoContext,
-#        private HttpHeaderRepository $repoHeader,
-#        private HttpBodyRepository $repoBody,
         private EntityManagerInterface $manager,
     ) {
     }
 
     public function recordRequest($tranId, Request $request)
     {
-        dd($request->headers->all());
         $createdAt = new \DateTimeImmutable();
 
         self::recordContext($this->manager, $tranId, Http::TYPE_SEND, $request->getMethod(), $request->getPathInfo(), 'TODO', $createdAt);
-
-        self::recordHeaders($this->manager, $tranId, Http::TYPE_SEND, []);
+        self::recordHeaders($this->manager, $tranId, Http::TYPE_SEND, $request->headers->all());
+        self::recordBody($this->manager, $tranId, Http::TYPE_SEND, $request->getContent());
     }
 
     public static function recordContext($manager, $tranId, $type, $f1, $f2, $f3, $createdAt)
