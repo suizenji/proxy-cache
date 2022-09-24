a6326a6ff92fa6b563b75679f1334dc5f7d07075
suizenji <suizenji@pop-os.localdomain>
2022-09-24 14:37:12 +0900

    prepare respnse

--- src/Service/CacheModerator.php
+++ src/Service/CacheModerator.php
@@ -3,19 +3,30 @@
 namespace App\Service;
 
 use App\Entity\CacheRule;
+use App\Entity\Http;
+use App\Entity\HttpContext;
+use App\Entity\HttpHeader;
+use App\Entity\HttpBodyt;
 use App\Repository\CacheRuleRepository;
+use App\Repository\HttpContextRepository;
+use App\Repository\HttpHeaderRepository;
+use App\Repository\HttpBodyRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CacheModerator
 {
-    public function __construct(private CacheRuleRepository $repo)
-    {
+    public function __construct(
+        private CacheRuleRepository $repoRule,
+        private HttpContextRepository $repoContext,
+        private HttpHeaderRepository $repoHeader,
+        private HttpBodyRepository $repoBody,
+    ) {
     }
 
     public function suggestResponse(Request $request): string|bool
     {
-        foreach ($this->repo->findAll() as $rule) {
+        foreach ($this->repoRule->findAll() as $rule) {
             $type = $rule->getType();
 
             if ($type === Entity::TYPE_SCHEME_HOST) {
@@ -30,6 +41,14 @@ class CacheModerator
 
     public function createResponse(string $cacheKey): Response
     {
+        $cond = ['tranId' => $cacheKey, 'type' => Http::TYPE_RECV];
+
+        $context = $this->repoContext->findOneBy($cond);
+        $headers = $this->repoHeader->findBy($cond);
+        $body = $this->repoBody->findOneBy($cond);
+
+        // TODO generate response
+
         return new Response('hello');
     }
 }
