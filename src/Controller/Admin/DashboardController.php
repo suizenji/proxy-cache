a7c808cc3e3ef1d4d83569d5ac5168a2574d9c1f
vagrant <vagrant@wsl.com>
2023-02-24 19:44:30 +0900

    fix dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,22 +7,15 @@ use App\Entity\HttpContext;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    #[Route('/_debug/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('@EasyAdmin/page/content.html.twig');
-        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
-        $url = $routeBuilder->setController(CacheRuleCrudController::class)->generateUrl();
-
-        return $this->redirect($url);
-
-        return parent::index();
+        return $this->render('/debug/admin.html.twig');
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
